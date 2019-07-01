51699f88a3c49e57fe6585db97109c0f8a7dead0
Julio Carpio <jjcarpio@correo.url.edu.gt>
2019-07-01 09:52:21 -0600

    Part 2 - Lab 4

--- webapps.tf
+++ webapps.tf
@@ -13,8 +13,9 @@ resource "random_string" "webapprnd" {
 }
 
 resource "azurerm_app_service_plan" "free" {
-  name                = "plan-free-${var.loc}"
-  location            = "${var.loc}"
+  count               = "${length(var.webapplocs)}"
+  name                = "plan-free-${var.webapplocs[count.index]}"
+  location            = "${var.webapplocs[count.index]}"
   resource_group_name = "${azurerm_resource_group.webapps.name}"
   tags                = "${azurerm_resource_group.webapps.tags}"
 
@@ -26,12 +27,14 @@ resource "azurerm_app_service_plan" "free" {
   }
 }
 
+//element(recurso.*.id, count.index) < Accede a un unico recurso de los creados a traves de un count. 
 resource "azurerm_app_service" "citadel" {
+  count               = "${length(var.webapplocs)}"
   name                = "webapp-${random_string.webapprnd.result}-${var.loc}"
   location            = "${var.loc}"
   resource_group_name = "${azurerm_resource_group.webapps.name}"
   tags                = "${azurerm_resource_group.webapps.tags}"
 
-  app_service_plan_id = "${azurerm_app_service_plan.free.id}"
+  app_service_plan_id = "${element(azurerm_app_service_plan.free.*.id, count.index)}"
 }
 
